efb6a85c3f734e9bda3b761faa0259dd1d2f1c54
XinHao <xinhaochin.dothouz@gmail.com>
2024-02-21 00:45:59 +0800

    update
    update

--- src/DetailsPage.js
+++ src/DetailsPage.js
@@ -61,7 +61,7 @@ const DetailsPage = () => {
       .then(res => res.json())
       .then(comment => {
         setComments(prevComments => [...prevComments, comment]);
-        console.log(comments);
+        // console.log(comments);
       })
       .catch(err => console.error(err));
   };
